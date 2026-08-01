3c0b224567accea3f5b3fb5174fa29d53968089b
Nathan Jacobson <nathan@nathanjacobson.ca>
2026-08-01 17:28:02 +0000

    added updated Desmos graph for tweaking speed curve

--- src/reactive/src/gap_follow_node.cpp
+++ src/reactive/src/gap_follow_node.cpp
@@ -172,7 +172,7 @@ double GapFollowNode::angle_to_speed_function(double angle)
     const double b = 1.0;
     const double c = 3.6;
 
-    double y = a * std::log(std::abs(angle) + b) + c; // current formula from Desmos tinkering <https://www.desmos.com/calculator/ijolz4pnpy>
+    double y = a * std::log(std::abs(angle) + b) + c; // current formula from Desmos tinkering <https://www.desmos.com/calculator/yeyvbi21nf>
     return std::clamp(y, min_speed, max_speed);
 }
 
